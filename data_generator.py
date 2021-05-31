780c79f47ef488e97a82ade5e3b77f835940342f
Mohamed Abdelgadir <mosaleh@tutanota.com>
2021-05-31 19:35:07 +0800

    random++

--- data_generator.py
+++ data_generator.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 
-SIZE = 214000
-
 
 class RandomDataGenerator:
     def __init__(self) -> None:
@@ -10,37 +8,37 @@ class RandomDataGenerator:
 
     def gen_integers(self,
                      low: int = 1,
-                     high: int = 9999999,
-                     size: int = SIZE) -> np.ndarray:
-        random_ints = self.rng.integers(low, high, size) # Integer Numbers
+                     high: int = 9999999) -> np.ndarray:
+        rand_size = self.get_random_size()
+        random_ints = self.rng.integers(low, high, rand_size) # Integer Numbers
         print("Memory size of random_ints numpy array in Byte: ", random_ints.nbytes)
 
         return random_ints
 
     def gen_real_numbers(self,
                      low: int = 1,
-                     high: int = 9999999,
-                     size: int = SIZE)  -> np.ndarray:
-        random_reals = np.round(self.rng.uniform(low, high, size), 3) # Real Numbers
+                     high: int = 9999999)  -> np.ndarray:
+        rand_size = self.get_random_size()
+        random_reals = np.round(self.rng.uniform(low, high, rand_size), 3) # Real Numbers
         print("Memory size of random_reals numpy array in Byte: ", random_reals.nbytes)
 
         return random_reals
 
     def gen_alphanumeric(self,
-                         length: int = 13,
-                         size: int = SIZE) -> np.ndarray:
+                         length: int = 13) -> np.ndarray:
+        rand_size = self.get_random_size()
         alpha_num = list('abcdefghijklmnopqrstuvwxyz0123456789')
-        np_alpha_num = np.random.choice(alpha_num, size=[size, length])
+        np_alpha_num = np.random.choice(alpha_num, size=[rand_size, length])
         random_alpha_num = np.array([''.join(i) for i in np_alpha_num])
         print("Memory size of random_alpha_num numpy array in Byte: ", random_alpha_num.nbytes)
 
         return random_alpha_num
 
     def gen_alphabetic(self,
-                       length: int = 10,
-                       size: int = SIZE) -> np.ndarray:
+                       length: int = 10) -> np.ndarray:
+        rand_size = self.get_random_size()
         alpha = list('abcdefghijklmnopqrstuvwxyz')
-        np_alpha = np.random.choice(alpha, size=[size, length])
+        np_alpha = np.random.choice(alpha, size=[rand_size, length])
         random_alpha = np.array([''.join(i) for i in np_alpha])
         print("Memory size of random_alpha numpy array in Byte: ", random_alpha.nbytes)
 
@@ -61,15 +59,35 @@ class RandomDataGenerator:
         self.rng.shuffle(np_arr)
         np_arr.tofile(filename, sep=sep, format=formats)
 
+    def get_random_size(self):
+        rand_size = self.rng.integers(1000, 10000)
 
+        return rand_size
 if __name__ == "__main__":
     rand = RandomDataGenerator()
 
-    ints = rand.gen_integers()
-    reals = rand.gen_real_numbers()
-    alpha = rand.gen_alphabetic()
-    alpha_num = rand.gen_alphanumeric()
+    combo = np.array([])
+    combo_size_bytes_min = 106500000
+    combo_size_bytes_max = combo_size_bytes_min + 1000000
 
-    combo = rand.combine_numpy_arrays([ints, reals, alpha, alpha_num])
+    try:
+        while True:
+            ints = rand.gen_integers()
+            reals = rand.gen_real_numbers()
+            alpha = rand.gen_alphabetic()
+            alpha_num = rand.gen_alphanumeric()
+            print("===========================")
+            combo = np.append(combo, ints)
+            combo = np.append(combo, reals)
+            combo = np.append(combo, alpha)
+            combo = np.append(combo, alpha_num)
 
-    rand.write_to_file(combo)
+            if combo.nbytes in range(combo_size_bytes_min, combo_size_bytes_max):
+                print(combo.nbytes)
+                rand.write_to_file(combo)
+                break
+
+    except KeyboardInterrupt:
+        print("Combo np Array is:", combo.nbytes)
+        rand.write_to_file(combo)
+        print("Combo np Array has been written to `output.txt` file!!", combo.nbytes)
